8ef03a44766b74fbff7f44dff11720ca61951af8
Ayush King <vibhanshugb17@gmail.com>
2024-05-22 16:07:43 +0530

    update

--- script.js
+++ script.js
@@ -2,18 +2,26 @@ const toggle_bar = document.getElementById('toggle-bar');
 const toggle_button = document.getElementById('toggle-button');
 const content = document.getElementById ('content');
 const footer = document.getElementById('footer');
+const page1R = document.getElementById('page1R');
+const page1L = document.getElementById('page1L');
+
 
 
 toggle_button.addEventListener("click",function (){
   toggle_bar.classList.toggle('toggle-bar-show');
   content.classList.toggle('content-show');
- 
+  footer.classList.toggle('footer_hide');
+  
   
   
   if (toggle_button.className === "toggle-button") {
     toggle_button.classList.toggle('toggle-button-on');
+    
   } else {
     toggle_button.classList.toggle('toggle-button-off');
+    page1R.classList.toggle('page1R_hide');
+    page1L.classList.toggle('page1L_hide');
+ 
   }
   
 });
@@ -22,7 +30,14 @@ toggle_button.addEventListener("click",function (){
 const home_btn = document.getElementById('home-button');
 
 home_btn.addEventListener("click",function () {
-  
+  toggle_button.classList.toggle('toggle-button-off');
+  toggle_bar.classList.toggle('toggle-bar-show');
+  content.classList.toggle('content-show');
+  if (page1.classList.toggle('page1-show')) {
+    page1.classList.toggle('page1-show');
+  } else {
+    content.classList.toggle('content-none');
+  }
 });
 
 const page1_btn = document.getElementById('page1-button');
@@ -30,8 +45,9 @@ const page1 = document.getElementById('page1');
 
 
 page1_btn.addEventListener("click",function () {
+  toggle_bar.classList.toggle('toggle-bar-show');
+  content.classList.toggle('content-show');
   page1.classList.toggle('page1-show');
   content.classList.toggle('content-none');
-  footer.classList.toggle =('hide-footer');
   
 });
