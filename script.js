a07db7cf7e381371c4cd5324158d88f4c5142a0c
Ayush King <vibhanshugb17@gmail.com>
2024-05-30 09:37:47 +0530

    update

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const toggle_bar = document.getElementById('toggle-bar');
 const toggle_button = document.getElementById('toggle-button');
 const content = document.getElementById ('content');
-const footer = document.getElementById('footer');
-const page1R = document.getElementById('page1R');
-const page1L = document.getElementById('page1L');
+const home_btn = document.getElementById('home-button');
+
 
 
 
@@ -27,27 +26,9 @@ toggle_button.addEventListener("click",function (){
 });
 
 
-const home_btn = document.getElementById('home-button');
-
 home_btn.addEventListener("click",function () {
   toggle_button.classList.toggle('toggle-button-off');
   toggle_bar.classList.toggle('toggle-bar-show');
   content.classList.toggle('content-show');
-  if (page1.classList.toggle('page1-show')) {
-    page1.classList.toggle('page1-show');
-  } else {
-    content.classList.toggle('content-none');
-  }
 });
 
-const page1_btn = document.getElementById('page1-button');
-const page1 = document.getElementById('page1');
-
-
-page1_btn.addEventListener("click",function () {
-  toggle_bar.classList.toggle('toggle-bar-show');
-  content.classList.toggle('content-show');
-  page1.classList.toggle('page1-show');
-  content.classList.toggle('content-none');
-  
-});
